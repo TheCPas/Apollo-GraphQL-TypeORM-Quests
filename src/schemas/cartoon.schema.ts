00be8986e398896892ca3b058bd49605a0fa2b51
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-03-24 11:42:41 +0100

    [FIX] Probleme typage number

--- src/schemas/cartoon.schema.ts
+++ src/schemas/cartoon.schema.ts
@@ -2,8 +2,8 @@ export const Cartoon = `{
   id: ID
   name: String
   description: String
-  nb_of_episodes: Number
-  nb_of_seasons: Number
+  nb_of_episodes: Int
+  nb_of_seasons: Int
   genres: [String]
   realisator: String
   author: String
